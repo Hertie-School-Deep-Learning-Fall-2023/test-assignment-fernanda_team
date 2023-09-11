f888168d09c05987da7ea98b27cee1cd322f3891
MariaFernandaOrtega <229757@students.hertie-school.org>
2023-09-11 09:33:27 +0200

    Update implement_me.py

--- implement_me.py
+++ implement_me.py
@@ -2,4 +2,21 @@
 def lcm(a, b):
   """compute Least Common Multiple between two integers."""
 
-  pass
+  if a > b:
+       greater = a
+  else:
+       greater = b
+
+  while(True):
+       if((greater % a == 0) and (greater % b == 0)):
+           lcm = greater
+           break
+       greater += 1
+
+  return lcm
+
+num1 = 54
+num2 = 24
+print("The L.C.M. is", lcm(num1, num2))
+
+
